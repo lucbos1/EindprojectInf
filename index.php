6b64e3747dc1b4b94df376ddeba59a8756928434
Pizzatent <125358887+Pizzatent@users.noreply.github.com>
2023-03-02 11:33:31 +0100

    e

--- index.php
+++ index.php
@@ -16,17 +16,15 @@ if (!$conn) {
 
 <!DOCTYPE html>
 <html lang="nl">
-
 <!-- In dit document staat de header die op elke pagina wordt weergegeven -->
-  <head>
-    <title>App</title>
-    <link rel="icon" type="image/x-icon" href="img/MM_Logo.ico">
-    <link rel="stylesheet" type="text/css" href="css/reset.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  </head>
-  <body>
+    <head>
+        <link rel="icon" type="image/x-icon" href="img/MM_Logo.ico">
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
     	<nav>
     	    <a href='index.php'>HOME</a>
     	    <div>
@@ -34,15 +32,13 @@ if (!$conn) {
     	      <a href="">Inloggen</a>
     	    </div>
     	</nav>
-<main>
-    <a href="">HOI</a>
-<h1>Welkom</h1>
-HHTEEEE
-<!-- In dit document staat de footer die op elke pagina wordt weergegeven -->
-</main>
-
-</body>
-
+        <main>
+            <a href="">HOI</a>
+            <h1>Welkom</h1>
+            HHTEEEE
+        <!-- In dit document staat de footer die op elke pagina wordt weergegeven -->
+        </main>
+    </body>
 </html>
 
 <?php $conn->close(); ?>
